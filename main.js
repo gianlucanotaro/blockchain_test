d969c262154e674f8ede63d3f7eec613e5c2d0bb
Gianluca Notaro <gianluca.notaro@aclevion.com>
2022-07-22 13:58:52 +0200

    refactor

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const {Blockchain, Transactions} = require('./blockchain');
 const SHA256 = require('crypto-js/sha256');
 
 class Transactions{
@@ -29,70 +30,6 @@ class Block {
 
         console.log("Block mined: " + this.hash);
     }
-}
-
-class Blockchain{
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 5;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock() {
-        return new Block("01/01/2022", "Genesis Block", "0");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    minePendingTransactions(miningRewardAddress) {
-        let block = new Block(Date.now(), this.pendingTransactions);
-        block.mineBlock(this.difficulty);
-
-        console.log("Block successfully mined!");
-        this.chain.push(block);
-
-        this.pendingTransactions = [new Transactions(null, miningRewardAddress, this.miningReward)];
-    }
-
-    createTransaction(transaction) {
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAddress(address) {
-        let balance = 0;
-
-        for (const block of this.chain) {
-            for (const transaction of block.transactions) {
-                if (transaction.fromAddress === address) {
-                    balance -= transaction.amount;
-                }
-
-                if (transaction.toAddress === address) {
-                    balance += transaction.amount;
-                }
-            }
-        }
-        return balance;
-    }
-
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
 
 
