4180d41a4f096799e310282a3c0f84ec41b4ee1e
Gianluca Notaro <gianluca.notaro@aclevion.com>
2022-07-22 11:47:28 +0200

    add Block functions

--- main.js
+++ main.js
@@ -23,4 +23,27 @@ class Blockchain{
     createGenesisBlock() {
         return new Block(0, "01/01/2022", "Genesis Block", "0");
     }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
 }
+
+
+let blockchain = new Blockchain();
+blockchain.addBlock(new Block(1, "01/01/2022", {amount: 4}));
+blockchain.addBlock(new Block(2, "01/01/2022", {amount: 10}));
+blockchain.addBlock(new Block(3, "01/01/2022", {amount: 20}));
+blockchain.addBlock(new Block(4, "01/01/2022", {amount: 40}));
+blockchain.addBlock(new Block(5, "01/01/2022", {amount: 80}));
+blockchain.addBlock(new Block(6, "01/01/2022", {amount: 160}));
+blockchain.addBlock(new Block(7, "01/01/2022", {amount: 320}));
+
+console.log(JSON.stringify(blockchain, null, 4));
