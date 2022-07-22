e4279a5028e061edcdcf104bc7537fe94f6b7e4a
Gianluca Notaro <gianluca.notaro@aclevion.com>
2022-07-22 11:44:14 +0200

    add simple first implementation of blockchain

---
+++ main.js
@@ -0,0 +1,26 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+}
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2022", "Genesis Block", "0");
+    }
+}
